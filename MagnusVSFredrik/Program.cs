ec5dbe3e9e7447980124e7d254693f3a95a612fa
Administrator <Administrator@ACADEMY-6303P80>
2017-03-22 16:40:33 +0100

    //Ändrat i loopen

--- MagnusVSFredrik/Program.cs
+++ MagnusVSFredrik/Program.cs
@@ -16,8 +16,10 @@ namespace MagnusVSFredrik
             for (int i = 0; i < 100; i++)
             {
                 Console.WriteLine(i);
-                Console.WriteLine("F..");
+                Console.WriteLine("M..");
             }
+
+            
         }
     }
 }
